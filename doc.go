d1bd4e7da0d44494604cf6cd0a46aa11f4b3b4f0
billyct <billy_allen@126.com>
2019-03-11 22:32:49 +0800

    🐰 fix gb2312 file.

--- doc.go
+++ doc.go
@@ -5,6 +5,8 @@ import (
 	"strings"
 	"github.com/PuerkitoBio/goquery"
 	"regexp"
+	"github.com/axgle/mahonia"
+	"unicode/utf8"
 )
 
 func ReadDoc(path string) (string, error){
@@ -14,7 +16,13 @@ func ReadDoc(path string) (string, error){
 		return "", err
 	}
 
-	doc, err := goquery.NewDocumentFromReader(strings.NewReader(string(s)))
+	reader := strings.NewReader(string(s))
+	if (!utf8.Valid(s)) {
+		// gb2312 转成 utf-8
+		reader = strings.NewReader(mahonia.NewDecoder("gbk").ConvertString(string(s)))
+	}
+
+	doc, err := goquery.NewDocumentFromReader(reader)
 	if err != nil {
 		return "", err
 	}
@@ -23,7 +31,7 @@ func ReadDoc(path string) (string, error){
 	text := doc.Find("body").Text()
 
 	// 替换 &nbsp;
-	text = strings.Replace(text, "\u00A0", "", -1)
+	text = strings.Replace(text, "\u00A0", " ", -1)
 	// 将所有 \s 换成一个空格
 	r, err := regexp.Compile("\\s+")
 	if err != nil {
